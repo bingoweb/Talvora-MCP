6d664ef530c5e36202c2a8b33b85600397a335f6
Taylan Soylu <6751394+bingoweb@users.noreply.github.com>
2026-09-13 23:56:35 +0300

    feat: bump broker protocol for start-only execution

--- src/Talvora.Ipc.Contracts/BrokerProtocol.cs
+++ src/Talvora.Ipc.Contracts/BrokerProtocol.cs
@@ -2,8 +2,8 @@ namespace Talvora.Ipc.Contracts;
 
 public static class BrokerProtocol
 {
-    public const int CurrentVersion = 1;
-    public const string DefaultPipeName = "Talvora.ElevatedBroker.v1";
+    public const int CurrentVersion = 2;
+    public const string DefaultPipeName = "Talvora.ElevatedBroker.v2";
     public const string PipeNameConfigurationKey = "Broker:PipeName";
     public const string AllowedUserSidConfigurationKey = "Broker:AllowedUserSid";
 }
